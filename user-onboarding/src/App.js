4d4d8973e349d7d667fe50f1b4c53bedaf50ea60
caralocke <cara.m.locke@gmail.com>
2021-05-19 11:11:10 -0400

    Completely set up my project and started working on Form.js as well as importing formSchema like shown

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -1,6 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
 import Form from './components/Form' //imported Form.js to my App.js
+import schema from './validation/formSchema'
+import axios from 'axios'
+import * as yup from 'yup'
+
 function App() {
   return (
     <div className="App">
