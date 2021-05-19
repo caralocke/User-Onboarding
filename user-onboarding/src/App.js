8799cea99aff0b2b12ab9b64cc315e76e7d69abf
caralocke <cara.m.locke@gmail.com>
2021-05-19 17:08:31 -0400

    working on trying to get today's project to work like how we did the GP

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react'
 import './App.css';
+import User from './components/User'
 import Form from './components/Form' //imported Form.js to my App.js
 import schema from './validation/formSchema'
 import axios from 'axios'
@@ -25,14 +26,95 @@ import * as yup from 'yup'
 
 function App() {
      //states
-     const [user, setUser] = useState(initialUser)
+     const [users, setUsers] = useState(initialUser)
      const [formValues, setFormValues] = useState(initialFormValues)
      const [formErrors, setFormErrors] = useState(initialFormErrors)
      const [disabled, setDisabled] = useState(initialDisabled)
 
+
+     //helpers
+     const getUsers = () => {
+      axios.get('https://reqres.in/api/users')
+      .then(res => {
+        console.log('res.data: \n', res.data.data)
+        setUsers(res.data.data)
+      })
+      .catch(err => {
+        debugger
+        console.log(err)
+      })
+     }
+
+     const postNewUser = newUser => {
+       axios.post('https://reqres.in/api/users', newUser)
+       .then(res => {
+         setFormValues(initialFormValues)
+       })
+       .catch(err => {
+         debugger
+         console.log(err)
+       })
+     }
+
+     const validate = (name, value) => {
+       yup
+       .reach(schema, name)
+       .validate(value)
+       .then( validate => {
+        setFormErrors({
+          ...formErrors,
+          [name]: ''
+        })
+       })
+     }
+
+     //event handlers
+     const inputChange = (name, value) => {
+       validate(name, value)
+       setFormValues({
+         ...formValues,
+         [name]: value
+       })
+     }
+
+     const formSubmit = () => {
+       const newUser = {
+         name:formValues.name.trim(),
+         email: formValues.email.trim(),
+         password: formValues.password,
+         terms: formValues.terms
+       }
+       postNewUser(newUser)
+     }
+
+     //side effects
+     useEffect(() => {
+       getUsers()
+     }, [])
+
+     useEffect(() => {
+       schema.isValid(formValues).then(valid => {
+         setDisabled(!valid)
+       })
+     }, [formValues])
+
   return (
     <div className="App">
-      <Form /> {/*placed my Form inside my JSX */}
+      <Form
+      values={formValues}
+      change={inputChange}
+      submit={formSubmit}
+      disabled={disabled}
+      errors={formErrors}
+      /> {/*placed my Form inside my JSX */}
+
+      {
+        users.map(user => {
+          return (
+            <User key={user.id} details={user}/>
+          )
+        })
+      }
     </div>
   );
 }
