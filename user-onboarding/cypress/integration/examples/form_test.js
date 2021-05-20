69fade7083311d829382dc1e02b5052c651e1e8e
caralocke <cara.m.locke@gmail.com>
2021-05-20 15:49:52 -0400

    Wrote a test to make sure the user could click the terms of service box

--- user-onboarding/cypress/integration/examples/form_test.js
+++ user-onboarding/cypress/integration/examples/form_test.js
@@ -6,12 +6,14 @@ describe('User Onboarding App', () => {
 
     const nameInput = () => cy.get('input[name=name]') // created a helper for nameInput
     const emailInput = () => cy.get('input[name=email]') //created a helper for emailInput
-    const passwordInput = () => cy.get('input[name=password]') //create a helper for passwordInput
+    const passwordInput = () => cy.get('input[name=password]') //created a helper for passwordInput
+    const termsInput = () => cy.get('input[name=terms]') //created a helper for termsInput
 
     it('Get an input and type something in it', () => {
         nameInput().type('Cara') //input a name
         nameInput().should('have.value', 'Cara') //use an assertion to check if the text inputted contains the name I provided
         emailInput().type('cara@cara.com') //input an email address
         passwordInput().type('abc123') //input a password
+        termsInput().click() //see if user can check the terms of service box
     })
 })
