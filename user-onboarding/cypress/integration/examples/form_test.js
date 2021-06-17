bcbc28de2265c16e773e001c55caf80ccbe8edc1
caralocke <cara.m.locke@gmail.com>
2021-06-17 10:47:53 -0400

    Created a password input helper function and created a test to input a password.

--- user-onboarding/cypress/integration/examples/form_test.js
+++ user-onboarding/cypress/integration/examples/form_test.js
@@ -8,11 +8,13 @@ describe('User Onboarding App', () => {
     //Create helper functions for each area I want to test
     const nameInput = () => cy.get('input[name=name]')
     const emailInput = () => cy.get('input[name=email]')
+    const passwordInput = () => cy.get('input[name=password]')
 
     it('Get an input and put something in it', () => {
         nameInput().type('Cara') //grab the name input and type a name into it
         nameInput().should('have.value', 'Cara') //test that the name input has the correct value
         emailInput().type('email@email.com') // put an email into the email input
         emailInput().should('have.value', 'email@email.com') //test that the email input has the correct value
+        passwordInput().type('password') //put a password into the password input
     })
 })
