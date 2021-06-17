3e33bbf28ccab5f0cb9f00d6115ecfe420f3726d
caralocke <cara.m.locke@gmail.com>
2021-06-17 10:41:48 -0400

    Used an assertion to check if the text input for name contained the name I provided.

--- user-onboarding/cypress/integration/examples/form_test.js
+++ user-onboarding/cypress/integration/examples/form_test.js
@@ -1,11 +1,15 @@
+//tell the test what I want to test (describe)
 describe('User Onboarding App', () => {
     beforeEach(() => {
+        //tell the test to go to a fresh version of my site each time
         cy.visit('http://localhost:3000')
     })
 
+    //Create helper functions for each area I want to test
     const nameInput = () => cy.get('input[name=name]')
 
     it('Get an input and put something in it', () => {
-        nameInput().type('Cara')
+        nameInput().type('Cara') //grab the name input and type a name into it
+        nameInput().should('have.value', 'Cara') //test that the name input has the correct value
     })
 })
